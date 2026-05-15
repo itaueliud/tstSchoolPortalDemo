43ea53345d3e66d7d478211b1bdeced72500f635
Bravin-lab <bravinlinyonyi@gmail.com>
2026-05-15 12:27:22 +0000

    added updates

--- src/chart.ts
+++ src/chart.ts
@@ -1,26 +1,3 @@
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend,
-  Filler,
-} from 'chart.js'
-
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend,
-  Filler
-)
+import ChartJS from 'chart.js/auto'
 
 export default ChartJS
